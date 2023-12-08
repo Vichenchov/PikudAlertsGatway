0bb1b40a21b7437f4e30183a0e36fe35b0d240be
Vichenchov <max.viva10@gmail.com>
2023-12-08 19:49:47 +0200

    changed model

--- alerts.model.js
+++ alerts.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema({
     city : String,
     time : Date,
+    area: String
 })
 
 module.exports = mongoose.model("Alerts", schema)
